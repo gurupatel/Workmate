155c97c11eb5838d28a921765a37a4e63aa262b2
Gaurang <gurupatel.2684@gmail.com>
2020-03-01 21:14:50 +0530

    - Initialized git repo - Created new swift project

---
+++ Workmate/Workmate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workmate
+//
+//  Created by Gaurang Patel on 01/03/20.
+//  Copyright © 2020 Workmate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
