c216bc7d95cf89f6091d87c0f8660ad322c2a44d
Gaurang <gurupatel.2684@gmail.com>
2020-03-01 21:54:17 +0530

    - Declared constants values like API's & method names - Added method for checking Internet connection

--- Workmate/Workmate/Constants.swift
+++ Workmate/Workmate/Constants.swift
@@ -12,6 +12,22 @@ import Alamofire
 
 struct Constants {
         
+    //--------------- Server Request URL's ---------------
+
+    static let authenticateUser: String = "https://api.helpster.tech/v1/auth/login/"
+
+    static let getWorkerDataAPI: String = "https://api.helpster.tech/v1/staff-requests/26074/"
+    
+    static let clockInAPI: String = "https://api.helpster.tech/v1/staff-requests/26074/clock-in/"
+
+    static let clockOutAPI: String = "https://api.helpster.tech/v1/staff-requests/26074/clock-out/"
+    
+    //--------------- Method Name ---------------
+    
+    static let getMethode: String = "GET"
+
+    static let postMethode: String = "POST"
+
     //MARK:- isConnectedToInternet Functions
 
     static func isConnectedToInternet() -> Bool {
